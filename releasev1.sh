861d9eb545f5de51a576a6d8cfc31c6b618d8006
lufeng <785035727@qq.com>
2018-05-24 15:51:17 +0800

    up

--- releasev1.sh
+++ releasev1.sh
@@ -2,7 +2,6 @@
 
 npm run build
 cd dist
-mv index.html mobile.html
-ossutil64.exe cp -r -f static oss://willdata/huiyi/production/static/
-# mv ./index.html ../../huiyizhan/src/main/webapp/templates/fed/mobile.html
-# cd -
+ossutilmac64 cp -r -f static oss://willdata/huiyi/production/static/
+mv ./index.html ../../huiyizhan/src/main/webapp/templates/fed/mobile.html
+cd -
--- src/views/detail.vue
+++ src/views/detail.vue
@@ -348,12 +348,68 @@ export default {
         });
       }
     },
-    goBuyTicket() {
-      const id = this.id;
-      const nextRoute = `/buyTicket/${id}`;
-      this.$router.push({
-        path: nextRoute,
+    copyToClipboard(elem) {
+      // create hidden text element, if it doesn't already exist
+      var targetId = "_hiddenCopyText_";
+      var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+      var origSelectionStart, origSelectionEnd;
+      if (isInput) {
+        // can just use the original source element for the selection and copy
+        target = elem;
+        origSelectionStart = elem.selectionStart;
+        origSelectionEnd = elem.selectionEnd;
+      } else {
+          // must use a temporary form element for the selection and copy
+          target = document.getElementById(targetId);
+          if (!target) {
+            var target = document.createElement("textarea");
+            target.style.position = "absolute";
+            target.style.left = "-9999px";
+            target.style.top = "0";
+            target.id = targetId;
+            document.body.appendChild(target);
+          }
+          target.textContent = elem.textContent;
+      }
+      // select the content
+      var currentFocus = document.activeElement;
+      target.focus();
+      target.setSelectionRange(0, target.value.length);
+
+      // copy the selection
+      var succeed;
+      try {
+          succeed = document.execCommand("copy");
+      } catch(e) {
+          succeed = false;
+      }
+      // restore original focus
+      if (currentFocus && typeof currentFocus.focus === "function") {
+          currentFocus.focus();
+      }
+
+      if (isInput) {
+          // restore prior selection
+          elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+      } else {
+          // clear temporary content
+          target.textContent = "";
+      }
+      this.$vux.alert.show({
+        title: '温馨提示',
+        content: '链接已复制，请先到PC端购票',
       });
+      return succeed;
+    },
+    /* eslint-enable */
+    goBuyTicket() {
+      const url = document.getElementById('url');
+      this.copyToClipboard(url);
+      // const id = this.id;
+      // const nextRoute = `/buyTicket/${id}`;
+      // this.$router.push({
+      //   path: nextRoute,
+      // });
     },
     addWatchMeeting() {
       // debugger;
